dd5e854751f6b8cd460a306c274b6c0ee2577edf
vrindratech <vrindratech@gmail.com>
2026-09-09 23:25:43 +0530

    Added microsoft clarity ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,7 +95,21 @@ export default function RootLayout({
             gtag('config', 'G-LN9SGDVNC0');
           `}
         </Script>
-
+{/* Microsoft Clarity */}
+  <Script id="microsoft-clarity" strategy="afterInteractive">
+    {`
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){
+          (c[a].q=c[a].q||[]).push(arguments)
+        };
+        t=l.createElement(r);
+        t.async=1;
+        t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "yfpykb27x5");
+    `}
+  </Script>
         <Navbar />
 
         {children}
